f51569954d7fb7793b99087336d41fecc045e405
biohackerellie <epkerns@gmail.com>
2024-07-08 07:49:03 -0600

    updates

--- config/.config/nvim/lua/ellie/packer.lua
+++ config/.config/nvim/lua/ellie/packer.lua
@@ -1,75 +1,76 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.6',
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
 		-- or                            , branch = '0.1.x',
-		requires = { { 'nvim-lua/plenary.nvim' } }
-	}
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-	use { "catppuccin/nvim", as = "catppuccin" }
+	use({ "catppuccin/nvim", as = "catppuccin" })
 
-
-	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-	use('nvim-treesitter/nvim-treesitter-textobjects')
-	use {
-		'vimpostor/vim-tpipeline',
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	use("nvim-treesitter/nvim-treesitter-textobjects")
+	use({
+		"vimpostor/vim-tpipeline",
 		config = function()
 			vim.g.tpipeline_autoembed = 0
 			vim.g.tpipeline_clearstl = 1
-		end
-	}
-	use('nvim-treesitter/playground')
+		end,
+	})
+	use("nvim-treesitter/playground")
 
-	use('m4xshen/autoclose.nvim')
-	use('mbbill/undotree')
-	use('tpope/vim-fugitive')
-	use {
-		'nvim-tree/nvim-tree.lua',
+	use("m4xshen/autoclose.nvim")
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
+	use({
+		"nvim-tree/nvim-tree.lua",
 		requires = {
-			'nvim-tree/nvim-web-devicons',
+			"nvim-tree/nvim-web-devicons",
 		},
-	}
+	})
 	use("JoosepAlviste/nvim-ts-context-commentstring")
 
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'nvimtree/nvim-web-devicons', opt = true }
-	}
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "nvimtree/nvim-web-devicons", opt = true },
+	})
 
 	use({
 		"aserowy/tmux.nvim",
-		config = function() return require("tmux").setup() end
+		config = function()
+			return require("tmux").setup()
+		end,
 	})
 
 	use("L3MON4D3/LuaSnip")
 	use("rafamadriz/friendly-snippets")
-	use('windwp/nvim-ts-autotag')
-	use('github/copilot.vim')
-	use('nvimtools/none-ls.nvim')
-	use('williamboman/mason.nvim')
-	use('hrsh7th/nvim-cmp')
-	use('hrsh7th/cmp-buffer')
-	use('hrsh7th/cmp-nvim-lsp')
-	use('hrsh7th/cmp-path')
-	use('hrsh7th/cmp-nvim-lua')
-	use('jose-elias-alvarez/null-ls.nvim')
-	use('MunifTanjim/prettier.nvim')
+	use("windwp/nvim-ts-autotag")
+	use("github/copilot.vim")
+	use("nvimtools/none-ls.nvim")
+	use("williamboman/mason.nvim")
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-nvim-lua")
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("MunifTanjim/prettier.nvim")
 	use("folke/trouble.nvim")
-	use('nvim-pack/nvim-spectre')
-	use('neovim/nvim-lspconfig')
+	use("nvim-pack/nvim-spectre")
+	use("neovim/nvim-lspconfig")
 	use("j-hui/fidget.nvim")
-	use('onsails/lspkind.nvim')
-	use('nvim-lua/plenary.nvim')
-	use('williamboman/mason-lspconfig.nvim')
+	use("onsails/lspkind.nvim")
+	use("nvim-lua/plenary.nvim")
+	use("williamboman/mason-lspconfig.nvim")
 	use("xiyaowong/nvim-transparent")
-
+	use("lewis6991/hover.nvim")
 	use({
-		'nosduco/remote-sshfs.nvim',
-		requires = { 'nvim-telescope/telescope.nvim' }
+		"nosduco/remote-sshfs.nvim",
+		requires = { "nvim-telescope/telescope.nvim" },
 	})
-	use('romgrk/barbar.nvim')
-end
-)
+	use("romgrk/barbar.nvim")
+end)
