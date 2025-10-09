de0b433a7787653f6ada744e09aab6799d09d40e
biohackerellie <ellie@epklabs.com>
2025-10-09 11:19:44 -0600

    kitty changes

--- config/.config/nvim/lua/ellie/plugins/notes.lua
+++ config/.config/nvim/lua/ellie/plugins/notes.lua
@@ -1,39 +1,41 @@
 local M = {
-	{
-		"dhruvasagar/vim-table-mode",
-		cmd = "TableModeToggle",
-		ft = { "markdown" },
-		keys = {
-			{ "<leader>mt", "<Cmd>TableModeToggle<CR>", desc = "MarkDown table mode toggle" },
-		},
-	},
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		opts = {},
-		cmd = "RenderMarkdown toggle",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
-		keys = {
-			{ "<leader>md", "<Cmd>RenderMarkdown toggle<CR>", desc = "Render Markdown" },
-		},
-	},
+  {
+    "dhruvasagar/vim-table-mode",
+    cmd = "TableModeToggle",
+    ft = { "markdown" },
+    keys = {
+      { "<leader>mt", "<Cmd>TableModeToggle<CR>", desc = "MarkDown table mode toggle" },
+    },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {},
+    cmd = "RenderMarkdown toggle",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+    keys = {
+      { "<leader>md", "<Cmd>RenderMarkdown toggle<CR>", desc = "Render Markdown" },
+    },
+  },
 
-	-- neorg
-	{
-		"nvim-neorg/neorg",
-		opts = {},
-	},
-	-- markdown preview
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-		keys = {
-			{ "<leader>mp", "<Cmd>MarkdownPreviewToggle<CR>", desc = "Markdown Preview" },
-		},
-	},
+  -- neorg
+  -- {
+  --   "nvim-neorg/neorg",
+  --   opts = {
+  --     build = "rockspec"
+  --   },
+  -- },
+  -- markdown preview
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      { "<leader>mp", "<Cmd>MarkdownPreviewToggle<CR>", desc = "Markdown Preview" },
+    },
+  },
 }
 
 return M
