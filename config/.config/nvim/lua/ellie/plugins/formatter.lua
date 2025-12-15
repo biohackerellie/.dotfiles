eff75ca8f145d35bb8639477662ca578a0253b77
biohackerellie <ellie@epklabs.com>
2025-12-15 11:06:43 -0700

    dms

--- config/.config/nvim/lua/ellie/plugins/formatter.lua
+++ config/.config/nvim/lua/ellie/plugins/formatter.lua
@@ -1,43 +1,51 @@
 return {
 
-  "stevearc/conform.nvim",
-  cmd = "ConformInfo",
-  event = { "BufWritePre" },
-  keys = {
-    {
-      "<leader>fd",
-      function()
-        require("conform").format({ async = true, lsp_format = "fallback" })
-      end,
-      mode = "",
-      desc = "[F]ormat [D]ocument",
-    },
-  },
-  config = function()
-    local conform = require("conform")
-    conform.setup({
-      extra_lang_opts = {
-        go = {
-          lsp_format = "last",
-        },
-        rust = {
-          lsp_format = "last",
-        },
-      },
-      formatters_by_ft = {
-        sh = { "shfmt" },
-        lua = { "stylua" },
-        go = { "goimports" },
-        gdscript = { "gdformat" },
-        python = { "ruff_fix", "ruff_format" },
-        rust = { "rustfmt" }
-
-      },
-      format_on_save = {
-        enabled = true,
-        lsp_fallback = true,
-        async = false,
-      },
-    })
-  end,
+	"stevearc/conform.nvim",
+	cmd = "ConformInfo",
+	event = { "BufWritePre" },
+	keys = {
+		{
+			"<leader>fd",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end,
+			mode = "",
+			desc = "[F]ormat [D]ocument",
+		},
+	},
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			extra_lang_opts = {
+				go = {
+					lsp_format = "last",
+				},
+				rust = {
+					lsp_format = "last",
+				},
+			},
+			formatters_by_ft = {
+				sh = { "shfmt" },
+				lua = { "stylua" },
+				go = { "goimports", "gofumpt" },
+				gdscript = { "gdformat" },
+				python = { "ruff_fix", "ruff_format" },
+				rust = { "rustfmt" },
+				templ = { "templ", "injected" },
+			},
+			format_on_save = function()
+				if
+					vim.bo.filetype == "templ"
+					and #vim.diagnostic.get(0, { severity = { min = vim.diagnostic.severity.ERROR } }) > 0
+				then
+					vim.lsp.buf.format({ async = true })
+					return nil
+				end
+				return {
+					timeout_ms = 500,
+					lsp_fallback = true,
+				}
+			end,
+		})
+	end,
 }
